81053029da0c628829ea85cb37ebd1cf7cd784dc
ciupa.kamil <194928@edu.p.lodz.pl>
2017-03-23 18:07:33 +0100

    Client) Added AskSqrt, ConvertTochar, ToBigEndian functions

--- client.c
+++ client.c
@@ -8,6 +8,14 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
+
+//bool IsBigEndian(char* package);
+void AskSqrt(double number);
+unsigned char* ConvertToChar(unsigned int i);
+unsigned char* ToBigEndian(unsigned char* i);
+
+
 
 int main (int agrc, char *argv[]) {
 	int sockfd;
@@ -15,15 +23,15 @@ int main (int agrc, char *argv[]) {
 	struct sockaddr_in address;
 	int result;
 	double tosqrt = 4;
-	
+
 	char ch[16];// = "a";
 
 	//memcpy(&ch,&tosqrt,sizeof(tosqrt));
-	sprintf(ch,"%f",tosqrt);
+	//sprintf(ch,"%f",tosqrt);
 
-	 printf("%s",ch); 
-	
-	
+	// printf("%s",ch);
+
+
 	/*  Create a socket for the client.  */
 
 	sockfd = socket (AF_INET, SOCK_STREAM, 0);
@@ -32,7 +40,7 @@ int main (int agrc, char *argv[]) {
 
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr ("127.0.0.1");
-	address.sin_port = htons (9879);
+	address.sin_port = htons (9878);
 	len = sizeof (address);
 
 	/*  Now connect our socket to the server's socket.  */
@@ -46,10 +54,45 @@ int main (int agrc, char *argv[]) {
 	}
 
 	/*  We can now read/write via sockfd.  */
-
+	AskSqrt(2.0);
 	write (sockfd, &ch, 1);
 	read (sockfd, &ch, 1);
-	printf ("char from server = %s\n", ch);
+	//printf ("char from server = %s\n", ch);
 	close (sockfd);
 	exit (0);
+}
+
+
+
+void  AskSqrt(double number)	{
+
+	unsigned int i = 012;
+	unsigned	char *id ;
+	id = (unsigned char*)malloc(i*sizeof(unsigned int));
+	id = ToBigEndian(ConvertToChar(i));
+	printf(" %d \n %d \n %d \n %d", id[0],id[1],id[2],id[3]);
+
+}
+
+unsigned char* ConvertToChar(unsigned int i) {
+
+	unsigned char *result;
+	result = (unsigned char*)malloc(i*sizeof(unsigned int));
+	result[0] = i & 0x000000ff;
+	result[1] = (i & 0x0000ff00) >> 8;
+	result[2] = (i & 0x00ff0000) >> 16;
+	result[3] = (i & 0xff000000) >> 24;
+	return result;
+}
+
+unsigned char* ToBigEndian(unsigned char* i) {
+
+	unsigned char *result;
+	result = (unsigned char*)malloc(sizeof(i));
+	result[0] = i[3];
+	result[1] = i[2];
+	result[2] = i[1];
+	result[3] = i[0];
+	return result;
+
 }
--- server.c
+++ server.c
@@ -9,7 +9,7 @@
 
 
 int main() {
-  
+
   int server_sock, client_sock;
   socklen_t server_len, client_len;
   struct sockaddr_in server_address;
@@ -19,26 +19,26 @@ int main() {
   server_sock = socket( AF_INET, SOCK_STREAM, 0);
   server_address.sin_family = AF_INET;
   server_address.sin_addr.s_addr = htonl (INADDR_ANY);
-  server_address.sin_port = htons (9879);
+  server_address.sin_port = htons (9878);
   server_len = sizeof(server_address);
-  
+
   //bind
   bind (server_sock, (struct sockaddr *) &server_address, server_len);
-  
+
   //listen
-  
+
   listen(server_sock, 5);
-  
+
   while(1) {
-    
+
     char ch[16];
-  printf ("server waiting\n");    
+  printf ("server waiting\n");
     //accept
-    
+
     client_len = sizeof (client_address);
     client_sock = accept (server_sock, (struct sockaddr *) &client_address, &client_len);
-    
-    
+
+
 		read (client_sock, &ch, 1);
 		printf("rozmiar%lu\n", sizeof(ch));
 		printf("\n %s \n", ch);
